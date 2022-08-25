28e1e01b0bbfdc8fbd51318dd9e1ef57177e5cf1
John Sauter <John_Sauter@systemeyescomputerstore.com>
2022-08-25 19:44:45 -0400

    remove shopping paths from the front end

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -9,12 +9,9 @@ function Nav() {
       return (
         <ul className="flex-row">
           <li className="mx-1">
-            <Link to="/orderHistory">
-              Order History
-            </Link>
-          </li>
-          <li className="mx-1">
-            {/* this is not using the Link component to logout or user and then refresh the application to the start */}
+            {/* this is not using the Link component to 
+              * logout or user and then refresh the application 
+              * to the start */}
             <a href="/" onClick={() => Auth.logout()}>
               Logout
             </a>
@@ -43,8 +40,8 @@ function Nav() {
     <header className="flex-row px-1">
       <h1>
         <Link to="/">
-          <span role="img" aria-label="shopping bag">🛍️</span>
-          -Shop-Shop
+          <span role="img" aria-label="go home">🛍️</span>
+          Matchmaker
         </Link>
       </h1>
 
